e85b7189f47868b8ceab80ea7cf27523346e9172
Muhammad <m.i.aljuburi@gmail.com>
2023-02-16 22:27:05 -0700

    notifications

---
+++ packages/plugin-notification-client/src/types.ts
@@ -0,0 +1,23 @@
+import type { PluginEventDef } from "@candor/core-types";
+
+export type NotificationPayload = {
+  id: string;
+  type: string;
+  title: string;
+  body: string;
+  dismissed: boolean;
+  createdAt: number;
+  read?: boolean;
+  link?: string;
+  metaData?: Record<string, unknown>;
+};
+
+export interface NotificationClientEvents extends PluginEventDef {
+  send: {};
+  receive: {};
+  publish: {};
+  subscribe: {};
+  emit: {
+    "/notification/connection": NotificationPayload;
+  };
+}
