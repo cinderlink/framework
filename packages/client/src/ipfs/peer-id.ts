59431e1415af592fa15a07a04451674817946d6a
Andrew Ewing <drew@phenocode.com>
2023-02-17 04:41:12 -0700

    ipfs peer id fix

--- packages/client/src/ipfs/peer-id.ts
+++ packages/client/src/ipfs/peer-id.ts
@@ -1,7 +1,8 @@
 import { keys } from "@libp2p/crypto";
 import { createFromPrivKey } from "@libp2p/peer-id-factory";
+import { PeerId } from "@libp2p/interface-peer-id";
 
-export async function createPeerID(seed: Uint8Array) {
+export async function createPeerID(seed: Uint8Array): Promise<PeerId> {
   const key = await keys.supportedKeys.ed25519.generateKeyPairFromSeed(seed);
   const peerId = await createFromPrivKey(key);
   return peerId;
