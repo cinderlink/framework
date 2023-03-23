7b7612b236babc8da80768843ef6ead48d1d25c7
Andrew Ewing <drew@phenocode.com>
2023-03-22 17:20:39 -0700

    moving swagmi & early access apps

--- apps/swagmi/src/lib/wallet/ConnectButton.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { Button } from '@candor/ui-kit/interactive';
-	import ConnectModal from '$lib/wallet/ConnectModal.svelte';
-</script>
-
-<ConnectModal let:modal>
-	<Button on:click={() => modal?.openModal()}>Connect</Button>
-</ConnectModal>
