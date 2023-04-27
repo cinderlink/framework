4e945aee185f6745d59d24c5bac1ee3e0bd3cced
Muhammad <m.i.aljuburi@gmail.com>
2023-04-27 14:53:21 -0700

    feat(debugging): logs for identity

--- packages/client/src/identity.ts
+++ packages/client/src/identity.ts
@@ -21,13 +21,19 @@ export class Identity<PluginEvents extends PluginEventDef = PluginEventDef> {
   async resolve(): Promise<IdentityResolved> {
     if (this.resolving) return this.resolving;
     this.resolving = new Promise(async (resolve) => {
-      console.info(`client/identity/resolve > resolving local identity`);
+      this.client.logger.info(
+        "client/identity/resolve",
+        "resolving local identity"
+      );
       const emptyResult: IdentityResolved = {
         cid: undefined,
         document: undefined,
       };
       let resolved = await this.resolveLocal().catch(() => emptyResult);
-      console.info(`client/identity/resolve > resolving ipns identity`);
+      this.client.logger.info(
+        "client/identity/resolve",
+        "resolving ipns identity"
+      );
       const ipns = await this.resolveIPNS().catch(() => emptyResult);
       if (
         !Object.keys(resolved.document?.schemas || {}).length ||
@@ -38,7 +44,10 @@ export class Identity<PluginEvents extends PluginEventDef = PluginEventDef> {
       ) {
         resolved = ipns;
       }
-      console.info(`client/identity/resolve > resolving server identity`);
+      this.client.logger.info(
+        "client/identity/resolve",
+        "resolving server identity"
+      );
       const server = await this.resolveServer().catch(() => emptyResult);
       if (
         !Object.keys(resolved.document?.schemas || {}).length ||
@@ -52,10 +61,11 @@ export class Identity<PluginEvents extends PluginEventDef = PluginEventDef> {
       this.cid = resolved?.cid;
       this.document = resolved?.document;
 
-      console.info(`client/identity/resolve > identity resolved`, {
+      this.client.logger.info("client/identity/resolve", "identity resolved", {
         cid: this.cid,
         document: this.document,
       });
+
       this.client.emit("/identity/resolved", {
         cid: this.cid,
         document: this.document,
@@ -98,7 +108,7 @@ export class Identity<PluginEvents extends PluginEventDef = PluginEventDef> {
         { recursive: true, timeout: 3000 }
       )) {
         const cid = link.split("/").pop();
-        console.info("IPNS resolve", { link, cid });
+        this.client.logger.info("IPNS", "resolve IPNS", { link, cid });
         if (cid) {
           const document = await this.client.dag
             .loadDecrypted<IdentityDocument>(CID.parse(cid), undefined, {
@@ -156,10 +166,11 @@ export class Identity<PluginEvents extends PluginEventDef = PluginEventDef> {
       }
     }
 
-    console.info(`client/identity/resolve > server identity resolved`, {
+    this.client.logger.info("client/identity", "server identity resolved", {
       cid,
       document,
     });
+
     return { cid, document };
   }
 
@@ -173,20 +184,24 @@ export class Identity<PluginEvents extends PluginEventDef = PluginEventDef> {
     forceRemote?: boolean;
   }) {
     if (!this.hasResolved || !cid) {
-      console.warn(`client/identity/save > identity has not been resolved`);
+      this.client.logger.error(
+        "client/identity/save",
+        "identity has not been resolved"
+      );
       return;
     }
     if (!this.client.ipfs.isOnline) {
-      console.warn(`client/identity/save > ipfs is offline`);
+      this.client.logger.error("client/identity/save", "ipfs is offline");
       return;
     }
     this.cid = cid;
     this.document = document;
-    console.info(`client/identity/save`, {
+    this.client.logger.info("client/identity/save", "saving identity", {
       cid,
       document,
       servers: this.client.peers.getServers(),
     });
+
     await localforage.setItem("rootCID", cid).catch(() => {});
     await this.client.ipfs.pin.add(cid, { recursive: true }).catch(() => {});
     await this.client.ipfs.name
@@ -201,9 +216,14 @@ export class Identity<PluginEvents extends PluginEventDef = PluginEventDef> {
       await Promise.all(
         this.client.peers.getServers().map(async (server) => {
           if (server.did) {
-            console.info(`client/identity/save > sending identity to server`, {
-              server,
-            });
+            this.client.logger.info(
+              "client/identity/save",
+              "sending identity to server",
+              {
+                server,
+              }
+            );
+
             await this.client.send<CinderlinkClientEvents>(
               server.peerId.toString(),
               {
@@ -212,9 +232,13 @@ export class Identity<PluginEvents extends PluginEventDef = PluginEventDef> {
               }
             );
           } else {
-            console.info(`client/identity/save > no did for server`, {
-              server,
-            });
+            this.client.logger.warn(
+              "client/identity/save",
+              "no did for server",
+              {
+                server,
+              }
+            );
           }
         })
       );
